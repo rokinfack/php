cab50a8905225ba42ec062f61e210a3adf63093c
rostand kinfack <kinfackrostand@gmail.com>
2022-06-13 19:01:17 +0200

    mon travail

---
+++ foreach.php
@@ -0,0 +1,15 @@
+
+<?php $tableau=[
+    'nom'=>'kinfack',
+    'prenom'=>'rostand',
+    'age'=>30,
+    'email'=>'rostandkana2@gmail.com'
+
+];
+foreach($tableau as $cle){
+    echo $cle;
+}
+/*foreach($tableau as $cle=>$value){
+    echo 'le   '.$cle.'  du visiteur est: '.$value .'</br>;
+
+}*/
